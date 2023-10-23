1098a3e2dba5488f850885908ae3d03cf67cf08a
Dedurin Yaros <dedurinaroslav@gmail.com>
2023-10-23 11:27:02 +0300

    test func

--- poke/tryingSomething.go
+++ poke/tryingSomething.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+func test() (a, b, c, d string) {
+	a = "a"
+	b = "b"
+	c = "c"
+	d = "d"
+	return a, b, c, d
+}
+
 func main() {
-	fmt.Println("hello world")
+	fmt.Print(test)
+
 }
